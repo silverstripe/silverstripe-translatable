846398d128835adfe56d369f71ca5fca27d2cc58
Niklas Forsdahl <niklas@creamarketing.com>
2012-11-12 15:58:17 +0200

    Correct translation for language dropdown field in link form

--- code/controller/TranslatableEditorToolbarExtension.php
+++ code/controller/TranslatableEditorToolbarExtension.php
@@ -3,7 +3,7 @@
 class TranslatableEditorToolbarExtension extends DataExtension {
 	
 	function updateLinkForm(&$form) {
-		$field = new LanguageDropdownField('Language', _t('PageURLSelectionField.LANGUAGE', 'Language'));
+		$field = new LanguageDropdownField('Language', _t('CMSMain.LANGUAGEDROPDOWNLABEL', 'Language'));
 		$field->setForm($form);
 		$form->Fields()->insertBefore($field, 'internal');
 		Requirements::javascript('translatable/javascript/HtmlEditorField.Translatable.js');
